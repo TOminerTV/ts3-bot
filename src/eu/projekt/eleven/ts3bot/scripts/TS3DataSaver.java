43a554d887069d2a72e98ee8114a87b5cb7f53fd
Tobias Selzer <Tobias.Selzer1@web.de>
2022-05-07 00:04:43 +0200

    Fixed wrong ts3 group ids

--- src/eu/projekt/eleven/ts3bot/scripts/TS3DataSaver.java
+++ src/eu/projekt/eleven/ts3bot/scripts/TS3DataSaver.java
@@ -17,69 +17,69 @@ public class TS3DataSaver
         ts3Data.put("slotsAmount", 64);
 //        ts3Data.put("slotsAmount", ts3Api.getHostInfo().getTotalMaxClients());
         ts3Data.put("playerAmount", ts3Api.getClients().size() - 1); // minus one because the bot counts himself also
-        ts3Data.put("supporterAmount", TS3ApiEnhanced.countGroupMember(504852) + TS3ApiEnhanced.countGroupMember(504894));
-        ts3Data.put("youtuberAmount", TS3ApiEnhanced.countGroupMember(504858));
-        ts3Data.put("streamerAmount", TS3ApiEnhanced.countGroupMember(504887));
+        ts3Data.put("supporterAmount", TS3ApiEnhanced.countGroupMember(515494) + TS3ApiEnhanced.countGroupMember(515531));
+        ts3Data.put("youtuberAmount", TS3ApiEnhanced.countGroupMember(515499));
+        ts3Data.put("streamerAmount", TS3ApiEnhanced.countGroupMember(515526));
 
         JSONObject games = new JSONObject();
         JSONObject ls = new JSONObject();
-        ls.put("playerAmount", TS3ApiEnhanced.countGroupMember(504874) + TS3ApiEnhanced.countGroupMember(513485));
+        ls.put("playerAmount", TS3ApiEnhanced.countGroupMember(515513) + TS3ApiEnhanced.countGroupMember(515536));
         games.put("ls", ls);
 
         JSONObject ets2 = new JSONObject();
-        ets2.put("playerAmount", TS3ApiEnhanced.countGroupMember(504875));
+        ets2.put("playerAmount", TS3ApiEnhanced.countGroupMember(515514));
         games.put("ets2", ets2);
 
         JSONObject rocketLeague = new JSONObject();
-        rocketLeague.put("playerAmount", TS3ApiEnhanced.countGroupMember(504890));
+        rocketLeague.put("playerAmount", TS3ApiEnhanced.countGroupMember(515529));
         games.put("rocketLeague", rocketLeague);
 
         JSONObject mc = new JSONObject();
-        mc.put("playerAmount", TS3ApiEnhanced.countGroupMember(504876));
+        mc.put("playerAmount", TS3ApiEnhanced.countGroupMember(515515));
         games.put("mc", mc);
 
         JSONObject pubg = new JSONObject();
-        pubg.put("playerAmount", TS3ApiEnhanced.countGroupMember(504877));
+        pubg.put("playerAmount", TS3ApiEnhanced.countGroupMember(515516));
         games.put("pubg", pubg);
 
         JSONObject rainbowSixSiege = new JSONObject();
-        rainbowSixSiege.put("playerAmount", TS3ApiEnhanced.countGroupMember(504878));
+        rainbowSixSiege.put("playerAmount", TS3ApiEnhanced.countGroupMember(515517));
         games.put("rainbowSixSiege", rainbowSixSiege);
 
         JSONObject gtav = new JSONObject();
-        gtav.put("playerAmount", TS3ApiEnhanced.countGroupMember(504879));
+        gtav.put("playerAmount", TS3ApiEnhanced.countGroupMember(515518));
         games.put("gtav", gtav);
 
         JSONObject conanExiles = new JSONObject();
-        conanExiles.put("playerAmount", TS3ApiEnhanced.countGroupMember(515284));
+        conanExiles.put("playerAmount", TS3ApiEnhanced.countGroupMember(515544));
         games.put("conanExiles", conanExiles);
 
         JSONObject forzaHorizon5 = new JSONObject();
-        forzaHorizon5.put("playerAmount", TS3ApiEnhanced.countGroupMember(515285));
+        forzaHorizon5.put("playerAmount", TS3ApiEnhanced.countGroupMember(515545));
         games.put("forzaHorizon5", forzaHorizon5);
 
         JSONObject csgo = new JSONObject();
-        csgo.put("playerAmount", TS3ApiEnhanced.countGroupMember(504881));
+        csgo.put("playerAmount", TS3ApiEnhanced.countGroupMember(515520));
         games.put("csgo", csgo);
 
         JSONObject dbd = new JSONObject();
-        dbd.put("playerAmount", TS3ApiEnhanced.countGroupMember(514735));
+        dbd.put("playerAmount", TS3ApiEnhanced.countGroupMember(515538));
         games.put("dbd", dbd);
 
         JSONObject amongUs = new JSONObject();
-        amongUs.put("playerAmount", TS3ApiEnhanced.countGroupMember(514736));
+        amongUs.put("playerAmount", TS3ApiEnhanced.countGroupMember(515539));
         games.put("amongUs", amongUs);
 
         JSONObject sot = new JSONObject();
-        sot.put("playerAmount", TS3ApiEnhanced.countGroupMember(514737));
+        sot.put("playerAmount", TS3ApiEnhanced.countGroupMember(515540));
         games.put("sot", sot);
 
         JSONObject valorant = new JSONObject();
-        valorant.put("playerAmount", TS3ApiEnhanced.countGroupMember(514738));
+        valorant.put("playerAmount", TS3ApiEnhanced.countGroupMember(515541));
         games.put("valorant", valorant);
 
         JSONObject garticphone = new JSONObject();
-        garticphone.put("playerAmount", TS3ApiEnhanced.countGroupMember(514739));
+        garticphone.put("playerAmount", TS3ApiEnhanced.countGroupMember(515542));
         games.put("garticphone", garticphone);
 
         ts3Data.put("games", games);
